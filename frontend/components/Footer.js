c5c1aff7b288cd5e5116aaf9920b914391c90639
Saurabh M <61317144+0xSaurabhx@users.noreply.github.com>
2024-10-18 14:53:38 +0530

    Update Footer.js

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer className="bg-black/80 backdrop-blur-md shadow-lg px-8 py-4 w-full text-center text-white fixed bottom-0 left-0 z-50">
       <p className="text-sm">
-        © {new Date().getFullYear()} ReAdGeN. All rights reserved.
+        © 2023 - {new Date().getFullYear()} ReadGen. All rights reserved.
       </p>
     </footer>
   );
